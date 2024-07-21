886396f83b89207cedaa9dafce899aa466f5e52b
Rizwaan1 <137213557+Rizwaan1@users.noreply.github.com>
2024-07-21 18:11:49 +0200

    .

--- Assets/Prefabs/Main Folders/Scripts/Computer.cs
+++ Assets/Prefabs/Main Folders/Scripts/Computer.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Computer : MonoBehaviour , IInteractable
+public class Computer : MonoBehaviour, IInteractable
 {
-
     public AudioSource source;
     public AudioClip ComputerSound;
+    public GameObject uiPanel; // Reference to the UI Panel that you want to show
+
     // Start is called before the first frame update
     void Start()
     {
-        //source = GetComponent<AudioSource>();
+        // source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Interact()
     {
+        // Play the computer sound
         source.PlayOneShot(ComputerSound);
 
+        // Unlock the mouse cursor
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
 
+        // Show the UI Panel
+        uiPanel.SetActive(true);
     }
 }
--- Assets/Prefabs/Main Folders/Scripts/PlayerController.cs
+++ Assets/Prefabs/Main Folders/Scripts/PlayerController.cs
@@ -14,6 +14,7 @@ public class PlayerController : MonoBehaviour
     private float xRotation = 0f;
 
     public Transform cameraTransform;
+    public bool Stasis;
 
     void Start()
     {
@@ -30,55 +31,67 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        isGrounded = controller.isGrounded;
-        if (isGrounded && velocity.y < 0)
+        if (!Stasis)
         {
-            velocity.y = -2f; // Small downward force to keep the player grounded
+            isGrounded = controller.isGrounded;
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f; // Small downward force to keep the player grounded
+            }
+
+            float moveX = Input.GetAxis("Horizontal");
+            float moveZ = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+            // Normalize the movement vector to prevent faster diagonal movement
+            if (move.magnitude > 1)
+            {
+                move.Normalize();
+            }
+
+            if (isGrounded)
+            {
+                controller.Move(move * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                // Apply reduced horizontal control in the air
+                Vector3 airMove = new Vector3(move.x * airControlFactor, 0, move.z * airControlFactor);
+                controller.Move(airMove * moveSpeed * Time.deltaTime);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
         }
-
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-
-        // Normalize the movement vector to prevent faster diagonal movement
-        if (move.magnitude > 1)
-        {
-            move.Normalize();
-        }
-
-        if (isGrounded)
-        {
-            controller.Move(move * moveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            // Apply reduced horizontal control in the air
-            Vector3 airMove = new Vector3(move.x * airControlFactor, 0, move.z * airControlFactor);
-            controller.Move(airMove * moveSpeed * Time.deltaTime);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (!Stasis)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
         }
+        
     }
 
     void LookAround()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!Stasis)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        transform.Rotate(Vector3.up * mouseX);
+            cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
+        }
+        
     }
 }
